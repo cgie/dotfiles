c9eb5556299d59210bad9943e0770daef9f3173a
Christian Gießen <cgie@informatik.uni-kiel.de>
2013-10-21 14:21:07 +0200

    removed deprecated stuff

---
+++ screenlayout/dualdigital.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --mode 1600x1200 --pos 1600x0 --rotate normal --output HDMI1 --mode 1600x1200 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output VGA1 --off
